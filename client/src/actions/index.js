7fac033e554fc662725cad59363f9ae1348118e0
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-24 15:38:38 -0400

    MVP

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -7,7 +7,7 @@ export const FETCH_ART_DATA_FAILURE = 'FETCH_ART_DATA_FAILURE'
 export function fetchData() {
     return dispatch => {
         dispatch({ type: FETCH_ART_DATA_START })
-        axios.get('https://api.harvardartmuseums.org/object?q=totalpageviews:0&size=10&apikey=44448ae0-f675-11e9-b330-79a4d45fba64')
+        axios.get('https://api.harvardartmuseums.org/object?classification=Fragments&sort=random&hasimage=1&apikey=44448ae0-f675-11e9-b330-79a4d45fba64')
              .then(res => {
                console.log('Response', res)
                 dispatch({ type: FETCH_ART_DATA_SUCCESS, payload: res.data.records })
@@ -18,4 +18,3 @@ export function fetchData() {
     }
 }
 
-//http://taco-randomizer.herokuapp.com/random/?full-taco=true
