cab0952ba1dbfe908491f9b9ae1fd70a6dfeac91
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-24 12:55:21 -0400

    passed the store to the Provider and added a fetchData action creator

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import logger from 'redux-logger'
@@ -11,5 +11,5 @@ import App from './App';
 
 const store = createStore(reducer, applyMiddleware(thunk, logger))
 
-ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
