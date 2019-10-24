df8469fc11898fbb2cfcc8359fb3d822b85889e4
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-10-24 13:02:23 -0400

    added switch case to the reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -8,6 +8,23 @@ const initialState = {
 
 export const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case FETCH_ART_DATA_START:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case FETCH_ART_DATA_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                objects: action.payload
+            }
+        case FETCH_ART_DATA_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
         default:
             return state
     }
