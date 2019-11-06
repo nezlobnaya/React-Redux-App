fc1f8a8164b99bfad9677a8f6b47866429977b54
Vladislav Burlutskiy <vlad.burlutskiy@icloud.com>
2019-11-05 20:45:03 -0500

    fixing an error in Images to deploy

--- client/src/components/Images/Images.js
+++ client/src/components/Images/Images.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import Image from '../Image/Image';
+import Image from '../Image/image';
 
 const Images = () => {
     const objects = useSelector(state => state.objects)
